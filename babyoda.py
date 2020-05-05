aec9bc5234dd80c21092c396f0d08a0d4f0bda55
hubgityhw <58375840+hubgityhw@users.noreply.github.com>
2020-05-05 10:53:39 +0100

    Update babyoda.py

--- babyoda.py
+++ babyoda.py
@@ -4,5 +4,5 @@ import requests
 url = 'https://upload.wikimedia.org/wikipedia/en/0/00/The_Child_aka_Baby_Yoda_%28Star_Wars%29.jpg'
 r = requests.get(url, allow_redirects=True)
 
-open('anime.srt', 'wb').write(r.content)
+open('babyoda.jpg', 'wb').write(r.content)
 
